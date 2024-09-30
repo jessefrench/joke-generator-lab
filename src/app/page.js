74c54418ca1f2c1bddb73d08eed13be06c55aae0
Jesse <frejes32@gmail.com>
2024-09-29 19:59:22 -0500

    Project complete

--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,41 @@
+'use client';
+
+import { useState } from 'react';
+import { Button } from 'react-bootstrap';
+import getJoke from '../api/jokeData';
+import JokeCard from '../components/JokeCard';
+
 function Home() {
+  const [btnText, setBtnText] = useState('Get A Joke');
+  const [joke, setJoke] = useState({});
+
+  const setButton = (text) => {
+    setBtnText(text);
+  };
+
+  const getAJoke = () => {
+    getJoke().then((obj) => {
+      setJoke({
+        setup: obj.setup,
+        punchline: obj.delivery,
+      });
+      setButton('Get Punchline');
+    });
+  };
+
   return (
-    <div
-      className="text-center d-flex flex-column justify-content-center align-content-center"
-      style={{
-        height: '90vh',
-        padding: '30px',
-        maxWidth: '400px',
-        margin: '0 auto',
-      }}
-    >
-      Welcome to Next JS!
-    </div>
+    <>
+      <JokeCard joke={joke} btnText={btnText} />
+      {btnText === 'Get A Joke' || btnText === 'Get A New Joke' ? (
+        <Button type="button" onClick={getAJoke}>
+          {btnText}
+        </Button>
+      ) : (
+        <Button type="button" onClick={() => setButton('Get A New Joke')}>
+          {btnText}
+        </Button>
+      )}
+    </>
   );
 }
 
